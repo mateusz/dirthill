a3debbf42dfee499ffb94dedd1cc3b4083dc5740
mateusz <m@emptyshore.org>
2023-02-19 15:47:25 +1300

    and some more work

--- cubetest/main.go
+++ cubetest/main.go
@@ -15,18 +15,24 @@ import (
 //go:embed grassblock.gltf
 var grassBlockBytes []byte
 
+//const w = 796
+//const h = 448
+
+const w = 480
+const h = 270
+
 type Game struct {
 	Library       *tetra3d.Library
 	Width, Height int
 	Scene         *tetra3d.Scene
-	Camera        *tetra3d.Camera
+	Camera        examples.BasicFreeCam
 	SystemHandler examples.BasicSystemHandler
 }
 
 func NewGame() *Game {
 	game := &Game{
-		Width:  796,
-		Height: 448,
+		Width:  w,
+		Height: h,
 	}
 
 	game.Init()
@@ -44,31 +50,46 @@ func (g *Game) Init() {
 
 	g.Library = library
 	cube := library.Scenes[0].Root.SearchTree().ByName("Cube").Models()[0]
-	camera := library.Scenes[0].Root.SearchTree().ByName("Camera").First().(*tetra3d.Camera)
+	//camera := library.Scenes[0].Root.SearchTree().ByName("Camera").First().(*tetra3d.Camera)
 
-	// Create a new Scene and name it.
-	g.Scene = tetra3d.NewScene("cube example")
-	g.Scene.World.LightingOn = false
+	g.Scene = tetra3d.NewScene("cubetest")
+	// Hides too many blocks I think
+	//g.Scene.World.FogRange = []float32{0.999, 1.00}
+	//g.Scene.World.FogOn = true
+	//g.Scene.World.FogColor = tetra3d.NewColor(60, 70, 80, 255)
+	//g.Scene.World.FogMode = tetra3d.FogTransparent
+
+	g.Camera = examples.NewBasicFreeCam(g.Scene)
+	// Need to set up camera view window to match the game window, this just copies the hw settings from blender
+	//g.Camera.Camera = camera
+	g.Camera.Camera.Resize(w, h)
+	g.Camera.Camera.SetNear(0.1)
+	g.Camera.Camera.SetFar(16.0)
+	g.Camera.Camera.SetFieldOfView(90)
+	g.Camera.Move(0, 1, 1)
 
 	g.SystemHandler = examples.NewBasicSystemHandler(g)
+	g.Scene.World.LightingOn = false
 
 	for i := 1; i <= 128; i++ {
-		c1 := tetra3d.NewModel(cube.Mesh.Clone(), "c1")
+		//c1 := tetra3d.NewModel(cube.Mesh.Clone(), "c1")
+		c1 := cube.Clone().(*tetra3d.Model)
 		g.Scene.Root.AddChildren(c1)
 		for j := 1; j <= 128; j++ {
+			// This is needed if StaticMerge is done
 			//c2 := tetra3d.NewModel(cube.Mesh.Clone(), "c2")
-			c2 := c1.Clone()
-			c2.Move(float64(i)*0.1, 0.0, -float64(j)*0.1)
-			c1.DynamicBatchAdd(c1.Mesh.MeshParts[0], c2.(*tetra3d.Model))
+			c2 := c1.Clone().(*tetra3d.Model)
+			c2.Move(float64(i), float64(i+j), -float64(j))
+			c1.DynamicBatchAdd(c1.Mesh.MeshParts[0], c2)
 		}
 	}
 
 	// Create a camera, move it back.
 	//g.Camera = tetra3d.NewCamera(g.Width, g.Height)
-	g.Camera = camera
+	//g.Camera = camera
 
 	// Again, we don't need to actually add the camera to the scenegraph, but we'll do it anyway because why not.
-	g.Scene.Root.AddChildren(g.Camera)
+	//g.Scene.Root.AddChildren(g.Camera)
 
 }
 
@@ -78,7 +99,7 @@ func (g *Game) Update() error {
 	//cube := g.Scene.Root.Get("Cube")
 	//cube.SetLocalRotation(cube.LocalRotation().Rotated(0, 1, 0, 0.05))
 
-	// Debug views
+	g.Camera.Update()
 
 	return g.SystemHandler.Update()
 }
@@ -97,7 +118,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	// Draw depth texture if the debug option is enabled; draw color texture otherwise.
 	screen.DrawImage(g.Camera.ColorTexture(), nil)
 
-	g.SystemHandler.Draw(screen, g.Camera)
+	g.SystemHandler.Draw(screen, g.Camera.Camera)
 
 	if g.SystemHandler.DrawDebugText {
 		txt := `F1 to toggle this text
